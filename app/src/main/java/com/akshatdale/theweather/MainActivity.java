6ddda117f342950e7ad901ba8473bc6ab33cc420
Akshat dale <akshatdale1611@gmail.com>
2022-09-01 12:49:52 +0530

    Removing get LATLONG API call and get data throw direct city name adding some textView sunrise time ,sunset time,wind speed and humidity and getting data from API and set to textviews

--- app/src/main/java/com/akshatdale/theweather/MainActivity.java
+++ app/src/main/java/com/akshatdale/theweather/MainActivity.java
@@ -28,16 +28,15 @@ import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity {
 
     EditText cityEditText;
-    TextView cityTextView,weatherConditionTextView,temperatureTextView,sunTextView,windTextView;
+    TextView cityTextView,weatherConditionTextView,temperatureTextView,textViewSunriseTime,textViewSunsetTime,textViewWindSpeed,textViewHumidityPercentage;
     ImageView weatherImageView;
     LinearLayout linearLayoutFirst;
     LinearLayoutManager layoutManagerForRecycleView;
     RecyclerView recyclerViewForecast;
-    String editTextCityName;
     String cityLatitude;
     String cityLongitude;
-    String currentTemperature;
-    String windSpeed;
+    String editTextCityName ,currentCity, currentTemperature, currentWeatherIconId,currentWindSpeed, currentWeatherDescriptionMain,currentHumidity,
+            sunriseToday,sunsetToday;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,11 +47,14 @@ public class MainActivity extends AppCompatActivity {
         cityTextView = findViewById(R.id.cityTextView);
         weatherConditionTextView = findViewById(R.id.weatherConditionTextView);
         temperatureTextView = findViewById(R.id.temperatureTextView);
-        sunTextView = findViewById(R.id.sunTextView);
-        windTextView = findViewById(R.id.windTextView);
+        textViewSunsetTime = findViewById(R.id.textViewSunsetTime);
+        textViewSunriseTime = findViewById(R.id.textViewSunriseTime);
+        textViewWindSpeed = findViewById(R.id.textViewWindSpeed);
+        textViewHumidityPercentage = findViewById(R.id.textViewHumidityPercentage);
         weatherImageView = findViewById(R.id.weatherImageView);
         linearLayoutFirst = findViewById(R.id.linearLayoutFirst);
         recyclerViewForecast = findViewById(R.id.recyclerViewForecast);
+//
 //        SETTING HORIZONTAL LIST LAYOUT
         layoutManagerForRecycleView = new LinearLayoutManager(MainActivity.this,LinearLayoutManager.HORIZONTAL,false);
 //        SETTING LAYOUT IN RECYCLER VIEW
@@ -68,60 +70,65 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    public void getLatLongCallAPI(View view){
-        editTextCityName = cityEditText.getText().toString();
-        RequestQueue requestQueue;
-        requestQueue = Volley.newRequestQueue(MainActivity.this);
-        Log.i("WEATHER_LATLONG",editTextCityName);
-        String LatLongURL ="https://api.openweathermap.org/geo/1.0/direct?q="+ editTextCityName +"&limit=1&appid=b63f3db52cd36ca5cc60f382ff723087";
-        JsonArrayRequest jsonArrayRequestLatLon = new JsonArrayRequest(Request.Method.GET, LatLongURL, null, new Response.Listener<JSONArray>() {
-            @Override
-            public void onResponse(JSONArray response) {
-                Log.i("WEATHER_LATLONG",response.toString());
-
-                for (int i = 0; i < response.length(); i++) {
-//                    if (!response.isNull(i)){
-//                        getWeatherDataCallAPI();
-//                    }else {
-//                        Log.i("WEATHER_LATLONG","LAT LONG NOT FOUND");
+//    public void getLatLongCallAPI(View view){
+//
+//        RequestQueue requestQueue;
+//        requestQueue = Volley.newRequestQueue(MainActivity.this);
+//        Log.i("WEATHER_LATLONG",editTextCityName);
+//        String LatLongURL ="https://api.openweathermap.org/geo/1.0/direct?q="+ editTextCityName +"&limit=1&appid=b63f3db52cd36ca5cc60f382ff723087";
+//        JsonArrayRequest jsonArrayRequestLatLon = new JsonArrayRequest(Request.Method.GET, LatLongURL, null, new Response.Listener<JSONArray>() {
+//            @Override
+//            public void onResponse(JSONArray response) {
+//                Log.i("WEATHER_LATLONG",response.toString());
+//
+//                for (int i = 0; i < response.length(); i++) {
+////                    if (!response.isNull(i)){
+////                        getWeatherDataCallAPI();
+////                    }else {
+////                        Log.i("WEATHER_LATLONG","LAT LONG NOT FOUND");
+////                    }
+//
+//                    try {
+//                        JSONObject jsonObject = response.getJSONObject(i);
+//                        String city = jsonObject.getString("name");
+////                        SETTING CITY NAME ON SCREEN
+//                         cityTextView.setText(city+"  ");
+////                         GETTING LAT LONG
+//                         cityLatitude = jsonObject.getString("lat");
+//                         cityLongitude = jsonObject.getString("lon");
+//
+//                        Log.i("WEATHER_LATLONG", city);
+//                        Log.i("WEATHER_LATLONG", cityLatitude);
+//                        Log.i("WEATHER_LATLONG", cityLongitude);
+//                    } catch (JSONException e) {
+//                        e.printStackTrace();
 //                    }
-
-                    try {
-                        JSONObject jsonObject = response.getJSONObject(i);
-                        String city = jsonObject.getString("name");
-//                        SETTING CITY NAME ON SCREEN
-                         cityTextView.setText(city+"  ");
-//                         GETTING LAT LONG
-                         cityLatitude = jsonObject.getString("lat");
-                         cityLongitude = jsonObject.getString("lon");
-
-                        Log.i("WEATHER_LATLONG", city);
-                        Log.i("WEATHER_LATLONG", cityLatitude);
-                        Log.i("WEATHER_LATLONG", cityLongitude);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("WEATHER",error.toString());
-            }
-        });
-        requestQueue.add(jsonArrayRequestLatLon);
+//                }
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                Log.e("WEATHER",error.toString());
+//            }
+//        });
+//        requestQueue.add(jsonArrayRequestLatLon);
 //        GETTING WEATHER DATA THROW LAT LONG
 //        if (cityLatitude != null && cityLongitude != null){
 //       }
+//    }
 
-    }
 
+//    GETTING DATA CURRENT WEATHER DATA FROM API
     public void getWeatherDataCallAPI(View view){
+//        GETTING CITY NAME FROM EDITTEXT
+                editTextCityName = cityEditText.getText().toString();
 //        GETTING WEATHER DATA THROW LAT LONG
         RequestQueue requestQueue;
         requestQueue = Volley.newRequestQueue(MainActivity.this);
-        String getWeatherURL = "https://api.openweathermap.org/data/2.5/weather?lat="+cityLatitude+"&lon="+cityLongitude+
-                "&appid=b63f3db52cd36ca5cc60f382ff723087&units=metric";
+//        String getWeatherURL = "https://api.openweathermap.org/data/2.5/weather?lat="+cityLatitude+"&lon="+cityLongitude+
+//                "&appid=b63f3db52cd36ca5cc60f382ff723087&units=metric";
+
+        String getWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + editTextCityName + "&appid=b63f3db52cd36ca5cc60f382ff723087&units=metric";
         Log.d("WEATHER_DATA", "The URL is :" + getWeatherURL);
 
         JsonObjectRequest jsonObjectRequestGetWeather = new JsonObjectRequest(Request.Method.GET,
@@ -132,20 +139,30 @@ public class MainActivity extends AppCompatActivity {
                 try {
 //                    GETTING DATA FROM API AND SETTING TO SCREEN
                     currentTemperature = response.getJSONObject("main").getString("temp");
-                    temperatureTextView.setText(currentTemperature+"°C ");
-//                    GETTING FROM ARRAY TO OBJECT AND GET STRING FROM OBJECT AND SET SETTEXT
+                    currentHumidity = response.getJSONObject("main").getString("humidity");
+                    sunriseToday = response.getJSONObject("sys").getString("sunrise");
+                    sunsetToday = response.getJSONObject("sys").getString("sunset");
+                    currentCity = response.getString("name");
+                    temperatureTextView.setText(String.format("%s°C", currentTemperature));
+//                    GETTING FROM ARRAY "weather" TO OBJECT AND GET STRING FROM OBJECT AND SET SETTEXT
                      JSONArray jsonArray = response.getJSONArray("weather");
                      JSONObject jsonObject = jsonArray.getJSONObject(0);
-                    String weatherDescription = jsonObject.getString("main");
-                    weatherConditionTextView.setText(weatherDescription+"  ");
-//                    GET WIND SPEED AND MULTIPLY WITH 3.6 TO CONVER KM/H
-                    windSpeed = response.getJSONObject("wind").getString("speed");
-                    double windSpeedKM = ((Double.parseDouble(windSpeed))*3.6 );
-                    windTextView.setText(String.valueOf(windSpeedKM)+" km/h ");
+
+                     currentWeatherDescriptionMain = jsonObject.getString("main");
+                     currentWeatherIconId = jsonObject.getString("icon");
+                     currentWindSpeed = response.getJSONObject("wind").getString("speed");
+                     double windSpeedKM = ((Double.parseDouble(currentWindSpeed))*3.6 );
+                     weatherConditionTextView.setText(currentWeatherDescriptionMain);
+                     textViewWindSpeed.setText(windSpeedKM +" km/h ");
+                     textViewHumidityPercentage.setText(currentHumidity + "% ");
+
 
                     Log.d("WEATHER_DATA", "The temperature is :" + currentTemperature);
-                    Log.d("WEATHER_DATA", "The description is :" + weatherDescription);
-                    Log.d("WEATHER_DATA", "The wind is :" + windSpeed);
+                    Log.d("WEATHER_DATA", "The WeatherIconId is :" + currentWeatherIconId);
+                    Log.d("WEATHER_DATA", "The Humidity is :" + currentHumidity);
+                    Log.d("WEATHER_DATA", "The description is :" + currentWeatherDescriptionMain);
+                    Log.d("WEATHER_DATA", "The wind is :" + windSpeedKM);
+                    Log.d("WEATHER_DATA", "The Sunrise/Sunset is :" + sunriseToday+"/"+sunsetToday);
                 } catch (JSONException e) {
                     Log.i("WEATHER_DATA","SOME ERROR IN RESPONSE WEATHER");
                     e.printStackTrace();
